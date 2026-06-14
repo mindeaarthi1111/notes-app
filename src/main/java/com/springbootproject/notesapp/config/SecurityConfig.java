39b4d26c88b7812461a4acb2333ace9dcab212d1
mindeaarthi1111 <mindeaarthi376@gmail.com>
2026-06-14 13:06:05 +0530

    Day 3: Added spring security basic login

--- src/main/java/com/springbootproject/notesapp/config/SecurityConfig.java
+++ src/main/java/com/springbootproject/notesapp/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.springbootproject.notesapp.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
@@ -12,7 +14,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf((csrf)->csrf.disable());
-        return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+        return http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
                 .build();
+
+       // return http.build();
     }
 }
